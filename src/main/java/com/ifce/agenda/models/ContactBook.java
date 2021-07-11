f5d3728fe52b96b4deb630bc6972f7dcf22b7ed1
marcusvtv <marcusvtv@gmail.com>
2021-07-11 15:23:39 -0300

    correcao contact book

--- src/main/java/com/ifce/agenda/models/ContactBook.java
+++ src/main/java/com/ifce/agenda/models/ContactBook.java
@@ -1,16 +1,9 @@
 package com.ifce.agenda.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -19,59 +12,5 @@ public class ContactBook {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
-	@OneToMany(mappedBy = "contactBook", cascade = CascadeType.ALL)
-	private List<Contact> contactList = new ArrayList<Contact>();
 
-	public void insertContact(Contact contato) {
-		contato.setContactBook(this);
-		contactList.add(contato);
-	}
-	
-	public Contact getContact(Integer id) {
-		return this.getContactList().get(id); //modificar para retornar dado um id
-	}
-	
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Contact> getContactList() {
-		return contactList;
-	}
-
-	public void setContactList(List<Contact> contactList) {
-		this.contactList = contactList;
-	}
-
-	public ContactBook() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public ContactBook(Integer id, String name, List<Contact> contactList) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.contactList = contactList;
-	}
-	
-	
-	
-	
-	
-	
 }
